f74f71f4a6880149240e020bc7ee4a68f444ea1c
CoKhanh <duongcokhanh17110315@gmail.com>
2021-08-22 21:04:38 +0700

    Installed react native paper

--- src/components/GlobalHeader/index.tsx
+++ src/components/GlobalHeader/index.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import { Appbar } from 'react-native-paper';
 
 
 const GlobalHeader = (props: any) => {
     return (
         <Appbar.Header>
-            <Appbar.Content title={props.title} titleStyle={styles.content}/>
-        </Appbar.Header>
+            <Appbar.BackAction onPress={() => { }} />
+            {/* <Appbar.Content title={props.title} style={{ marginLeft: 0 }} titleStyle={{ marginLeft: 0, backgroundColor: 'red', color: 'green' }}/> */}
+            {/* <Appbar.Action icon="magnify" onPress={() => { }} /> */}
+            <Appbar.Content
+                title={props.title}
+                style={styles.content}
+                titleStyle={{ alignSelf: 'center' }}
+            />
+            {/* <Appbar.Action/> */}
+        </Appbar.Header>    
     )
 }
 
 const styles = StyleSheet.create({
     content: {
-        marginLeft: 'auto', marginRight: 'auto',
+        marginLeft: 0, position: 'absolute', left: 0, right: 0, zIndex: -1
     },
 })
 
